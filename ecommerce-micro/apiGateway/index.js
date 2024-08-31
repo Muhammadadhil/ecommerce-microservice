2132aee7e146523125febae3d4db5f2c2a420f55
muhammad adhil <muhammadadhil934@gmail.com>
2024-08-31 21:54:13 +0530

    set up api-gateway

--- ecommerce-micro/apiGateway/index.js
+++ ecommerce-micro/apiGateway/index.js
@@ -1,27 +1,27 @@
-import express from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import { createProxyMiddleware } from 'http-proxy-middleware';
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
-const app=express();
+const app = express();
 
 app.use(cors());
 app.use(helmet()); // Add security headers
-app.use(morgan("combined")); // Log HTTP requests
+app.use(morgan("combined"));
 
-const paths={
-    auth:'http://localhost:3030',
-    product:'http://localhost:4040',
-    order:'http://localhost:5050',
-    notification:'http://localhost:6060'
-}
+const paths = {
+    auth: "http://localhost:3030",
+    product: "http://localhost:4040",
+    order: "http://localhost:5050",
+    notification: "http://localhost:6060",
+};
 
-app.use('/auth',createProxyMiddleware({target:paths.auth,changeOrigin:true}));
-app.use('/product',createProxyMiddleware({target:paths.product,changeOrigin:true}))
+app.use("/auth", createProxyMiddleware({ target: paths.auth, changeOrigin: true }));
+app.use("/product", createProxyMiddleware({ target: paths.product, changeOrigin: true }));
 
-const port=process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`API Gateway is running on the port ${port}`);
-})
+});
